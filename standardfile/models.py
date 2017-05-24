dbad5c0e72e6cb518be4311a7a463fce5ce22c53
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2017-05-24 12:40:36 +0200

    Decrypt Tag item content as well

--- standardfile/models.py
+++ standardfile/models.py
@@ -6,6 +6,35 @@ from Crypto.Cipher import AES
 from binascii import hexlify, unhexlify
 
 
+def decrypt_001(data, master_key):
+    enc_item_key = base64.b64decode(data['enc_item_key'])
+    iv = enc_item_key[:16]
+    aes = AES.new(unhexlify(master_key), AES.MODE_CBC, iv)
+    item_key = aes.decrypt(enc_item_key)        # 144 bytes hex encoded data result
+    item_key = item_key[16:-ord(item_key[-1])]  # 112 bytes hex encoded data remain (without IV and padding)
+
+    item_key = '\0'*8 + unhexlify(item_key)
+    enc_key = item_key[8:len(item_key) / 2 + 8]
+    auth_key = item_key[len(item_key) / 2:]
+
+    hmac_sha256 = hmac.new(auth_key, data['content'], hashlib.sha256)
+
+    if hmac_sha256.hexdigest() != data['auth_hash']:
+        raise AuthenticationError("Could not verify authentication hash")
+
+    content = base64.b64decode(data['content'][3:])
+    iv = content[:16]
+    aes = AES.new(enc_key, AES.MODE_CBC, iv)
+    return aes.decrypt(content)
+
+
+def decrypt(data, master_key):
+    if data['content'][:3] == '001':
+        return decrypt_001(data, master_key)
+
+    raise RuntimeError("002 not supported yet")
+
+
 class Item(object):
     def __init__(self, data):
         self.uuid = data['uuid']
@@ -14,39 +43,24 @@ class Item(object):
         self.content_type = data['content_type']
 
     def __repr__(self):
-        return "<Item:created={}, type={}>".format(self.created, self.content_type)
+        return "<Item:type={}, created={}, updated={}>".format(self.content_type, self.created, self.updated)
 
 
-class Note(Item):
+class EncryptedItem(Item):
     def __init__(self, data, master_key):
-        super(Note, self).__init__(data)
+        super(EncryptedItem, self).__init__(data)
+        self.content = decrypt(data, master_key)
 
-        if not 'content' in data:
-            raise ValueError("Note must contain `content' key")
 
-        if data['content'][:3] == '001':
-            enc_item_key = base64.b64decode(data['enc_item_key'])
-            iv = enc_item_key[:16]
-            aes = AES.new(unhexlify(master_key), AES.MODE_CBC, iv)
-            item_key = aes.decrypt(enc_item_key)        # 144 bytes hex encoded data result
-            item_key = item_key[16:-ord(item_key[-1])]  # 112 bytes hex encoded data remain (without IV and padding)
+class Note(EncryptedItem):
+    def __init__(self, data, master_key):
+        super(Note, self).__init__(data, master_key)
 
-            item_key = '\0'*8 + unhexlify(item_key)
-            enc_key = item_key[8:len(item_key) / 2 + 8]
-            auth_key = item_key[len(item_key) / 2:]
 
-            hmac_sha256 = hmac.new(auth_key, data['content'], hashlib.sha256)
-        
-            if hmac_sha256.hexdigest() != data['auth_hash']:
-                raise AuthenticationError("Could not verify authentication hash")
-
-            content = base64.b64decode(data['content'][3:])
-            iv = content[:16]
-            aes = AES.new(enc_key, AES.MODE_CBC, iv)
-            d = aes.decrypt(content)
-
-    def __repr__(self):
-        return "<Note:created={}, updated={}>".format(self.created, self.updated)
+class Tag(EncryptedItem):
+    def __init__(self, data, master_key):
+        super(Tag, self).__init__(data, master_key)
+        print self.content
 
 
 def make(data, master_key):
@@ -55,5 +69,8 @@ def make(data, master_key):
 
     if data['content_type'] == 'Note':
         return Note(data, master_key)
+
+    if data['content_type'] == 'Tag':
+        return Tag(data, master_key)
 
     return Item(data)
